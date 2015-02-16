347201daf205104f7f598dd89baaab85190c7886
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-02-16 18:03:15 +0100

    better test case

--- src/test/java/com/apporiented/algorithm/clustering/WeightedClusteringAlgorithmTest.java
+++ src/test/java/com/apporiented/algorithm/clustering/WeightedClusteringAlgorithmTest.java
@@ -16,29 +16,22 @@
 
 package com.apporiented.algorithm.clustering;
 
-import static com.apporiented.algorithm.clustering.SampleClusterData.*;
-
-import javax.swing.JFrame;
-
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.apporiented.algorithm.clustering.visualization.DendrogramPanel;
-
 public class WeightedClusteringAlgorithmTest {
+	public static final double[][] DISTANCES = new double[][] { // A--B-C--D
+	{ 0, 2, 3, 5 }, { 2, 0, 1, 3 }, { 3, 1, 0, 2 }, { 5, 3, 2, 0 } };
+	public static final String[] NAMES = new String[] { "A", "B", "C", "D" };
+	public static final double[] WEIGHTSA = new double[] { 1, 100, 1, 1 }; // weight on B, D should be alone
+	public static final double[] WEIGHTSD = new double[] { 1, 1, 100, 1 };// weight on C, A should be alone
 
-  @Test
-  public void testClusteringAvgLink() {
-    ClusteringAlgorithm alg = new DefaultClusteringAlgorithm();
-    Cluster c = alg.performWeightedClustering(DISTANCES, NAMES, WEIGHTS,
-        new WeightedLinkageStrategy());
-    DendrogramPanel dp = new DendrogramPanel();
-    dp.setModel(c);
-    JFrame f = new JFrame();
-    f.setContentPane(dp);
-    f.pack();
-    f.setVisible(true);
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    Assert.assertNotNull(c);
-  }
+	@Test
+	public void testClusteringAvgLink() {
+		ClusteringAlgorithm alg = new DefaultClusteringAlgorithm();
+		Cluster ca = alg.performWeightedClustering(DISTANCES, NAMES, WEIGHTSA, new WeightedLinkageStrategy());
+		Assert.assertEquals("D", ca.getChildren().get(0).getName());
+		Cluster cd = alg.performWeightedClustering(DISTANCES, NAMES, WEIGHTSD, new WeightedLinkageStrategy());
+		Assert.assertEquals("A", cd.getChildren().get(0).getName());
+	}
 }
